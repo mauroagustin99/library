fa16dfe2cebb3115846678d61c54bf1e79d4b021
Mauro Marcosano <mauromarcosano@gmail.com>
2024-05-12 14:38:56 -0300

    Add remove book functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Library array
 const myLibrary = [];
 
+// Books container DOM
+const booksContainer = document.getElementById('books-container');
+
+// Book constructor
 function Book(title, author, pagesNumber, read) {
   this.title = title;
   this.author = author;
@@ -34,23 +39,77 @@ function displayLibrary() {
     const readParagraph = document.createElement('p');
     readParagraph.textContent = `¿Ya leído? ${book.read ? 'Sí' : 'No'}`;
 
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = 'Remove';
+
     bookDiv.appendChild(titleHeading);
     bookDiv.appendChild(authorHeading);
     bookDiv.appendChild(pagesParagraph);
     bookDiv.appendChild(readParagraph);
+    bookDiv.appendChild(removeBtn);
 
     booksContainer.appendChild(bookDiv);
+
+    //Adding 'remove' button functionality
+
+    removeBtn.addEventListener('click', () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      displayLibrary();
+    });
   });
 }
 
-const booksContainer = document.getElementById('books-container');
-
+// Book examples
 const book1 = new Book('el senior de los anillos', 'carlos', 123, true);
 const book2 = new Book('el seniors', 'roberto', 215, true);
-const book3 = new Book('el seniors', 'roberto', 215, true);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
-addBookToLibrary(book3);
-displayLibrary();
 
-console.log(myLibrary);
+//Adding 'new book' button functionality
+const showButton = document.getElementById('new-book');
+const showDialog = document.getElementById('new-book-dialog');
+
+const bookForm = document.getElementById('book-form');
+const bookNameInput = document.getElementById('book-name');
+const authorNameInput = document.getElementById('author-name');
+const pagesNumberInput = document.getElementById('pages-number');
+const readYesInput = document.getElementById('read-yes');
+
+showButton.addEventListener('click', () => {
+  showDialog.showModal();
+});
+
+bookForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const title = bookNameInput.value;
+  const author = authorNameInput.value;
+  const pagesNumber = parseInt(pagesNumberInput.value);
+  const read = readYesInput.checked;
+
+  // Create a new book instance
+  const newBook = new Book(title, author, pagesNumber, read);
+
+  // Add book to the library
+  addBookToLibrary(newBook);
+
+  // Show the library updated
+  displayLibrary();
+
+  // Reset the form
+  bookForm.reset();
+
+  // Close the dialog
+  showDialog.close();
+});
+
+const cancelBtn = document.getElementById('cancelBtn');
+cancelBtn.addEventListener('click', function () {
+  showDialog.close();
+
+  // Reset the form
+  bookForm.reset();
+});
+
+displayLibrary();
